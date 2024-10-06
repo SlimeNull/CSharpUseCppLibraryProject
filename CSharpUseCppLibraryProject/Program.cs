22ecdf46390768e4cec9984d8f2e13e143c4ba98
SlimeNull <slimenull@outlook.com>
2024-10-07 04:10:22 +0800

    Add project files.

---
+++ CSharpUseCppLibraryProject/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Runtime.InteropServices;
+
+namespace CSharpUseCppLibraryProject
+{
+    internal class Program
+    {
+        [DllImport("TestDll")]
+        static extern int Plus(int a, int b);
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
